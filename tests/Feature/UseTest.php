918a5970b3f85d8c4bc0bb7cb0a9191fae26faaa
majid nazari <majidnazarister@gmail.com>
2021-11-25 16:00:23 +0330

    add and refactor code user test

--- tests/Feature/UseTest.php
+++ tests/Feature/UseTest.php
@@ -18,47 +18,108 @@ class UseTest extends TestCase
      *
      * @return void
      */
-    public function test_w1()
-    {
-        // $date=[
-        //     "first_name" => "majid",
+    public function test_UserFetchAll()
+    {    
+        // $arrayValues = ['admin', 'acceptor', 'financial','manager'];
+        // $email= $this->faker->unique()->safeEmail();
+        // $mobile=$this->faker->regexify('09[0-9]{9}');
+        // $user=[
+        //     'first_name' => $this->faker->firstName,//"majid",
+        //     'last_name' => $this->faker->lastName,//"hamidey",
+        //     'mobile' =>  $mobile,
+        //     'type' => $arrayValues[rand(0,3)],
+        //     'email' => $email,
+        //     //'email_verified_at' => now(),
+        //     'password' => '12345', // password
+        //     //'remember_token' => Str::random(10),
         // ];
-        // $user = User::factory()->create([
-        //     'first_name' => "abdol". $this->faker->name(),
-        //     //"password" =>  Hash::make("12345"),
-        // ]);
-        // $response = $this->post(route('login'), [
-        //     'email' => $user->email,
-        //     'password' => 'password'
-        // ]);
+        $user=self::UserData();
+        $response_create = $this->post(route('User.store'), $user );         
+        $response_getAll = $this->get(route('User.index')); 
+       
+       $response_getAll->assertSee($user["email"]);
+       $response_getAll->assertSee($user["mobile"]);
+    }
+    public function test_UserStore()
+    {        
+        // $arrayValues = ['admin', 'acceptor', 'financial','manager'];
+        // $email= $this->faker->unique()->safeEmail();
+        // $mobile=$this->faker->regexify('09[0-9]{9}');
+        // $user=[
+        //     'first_name' => $this->faker->firstName,//"majid",
+        //     'last_name' => $this->faker->lastName,//"hamidey",
+        //     'mobile' =>  $mobile,
+        //     'type' => $arrayValues[rand(0,3)],
+        //     'email' => $email,
+        //     //'email_verified_at' => now(),
+        //     'password' => '12345', // password
+        //     //'remember_token' => Str::random(10),
+        // ];
+        $user=self::UserData();
+      // dd($user["mobile"]);
+        $response = $this->post(route('User.store'), $user );  
+       // dd($response["id"]);    
+        //$users = User::factory()->count(3)->make();       
+        $this->assertGreaterThan(0,User::all()->count());        
+       // $this->assertDatabaseCount('users', 1);
+       $this->assertDatabaseHas('users', [
+        'mobile' => $user["mobile"],
+        'email' => $user["email"]
+        ]);
+        $user_response = User::where('email', $user["email"])->where('mobile', $user["mobile"])->first();
+         $this->assertNotNull($user_response);        
+    }
+    public function test_UserUpdate()
+    {   
+        //$userExistId=User::where('id', '>', 0)->firstOrFail();    
+        $userExistId=User::where('id','>' ,0)->firstOrFail();
+       // dd($userExistId);
+        //$arrayValues = ['admin', 'acceptor', 'financial','manager'];
+        // $email= $this->faker->unique()->safeEmail();
+        // $mobile=$this->faker->regexify('09[0-9]{9}');
+        // $user=[
+        //     'first_name' => $this->faker->firstName,//"majid",
+        //     'last_name' => $this->faker->lastName,//"hamidey",
+        //     'mobile' =>  $user["mobile"],
+        //     'type' => $arrayValues[rand(0,3)],
+        //     'email' => $user["email"],           
+        //     'password' => '12345', // password            
+        // ];
+        $user=self::UserData();
+        $response = $this->post(route('User.store'), $user );
+        $anotherUser=self::UserData();
+       // $email= $this->faker->unique()->safeEmail();
+        //$mobile=$this->faker->regexify('09[0-9]{9}');
+        $response = $this->put(route('User.update', $response['id']),$anotherUser);       
+        // $this->assertDatabaseHas('users', [
+        //     'mobile' =>  $mobile,
+        //     'email' => $email
+        //     ]);
+        $user = User::where('email', $user["email"])->where('mobile', $user["mobile"])->first();
+         $this->assertNotNull($user);
+        // $this->assertAuthenticatedAs($user);
+    }
+    public function test_UserDelete()
+    {    
+        $userExistId=User::where('id','>' ,0)->firstOrFail();       
+        $response = $this->delete(route('User.destroy',$userExistId->id));        
+        $UserFound= User::withTrashed()->find($userExistId->id);  
+       //dd($UserFound);
+        $this->assertSoftDeleted($UserFound);      
+    }
+    public  function  UserData()
+    {
         $arrayValues = ['admin', 'acceptor', 'financial','manager'];
         $email= $this->faker->unique()->safeEmail();
         $mobile=$this->faker->regexify('09[0-9]{9}');
         $user=[
             'first_name' => $this->faker->firstName,//"majid",
-            'last_name' => $this->faker->firstName,//"hamidey",
+            'last_name' => $this->faker->lastName,//"hamidey",
             'mobile' =>  $mobile,
             'type' => $arrayValues[rand(0,3)],
-            'email' => $email,
-            //'email_verified_at' => now(),
-            'password' => '12345', // password
-            //'remember_token' => Str::random(10),
+            'email' => $email,           
+            'password' => '12345', // password            
         ];
-        $response = $this->post(route('User.store'), $user );
-       // $response->assertRedirect(route('User.index'));
-        //$this->assertAuthenticatedAs($user);
-        //$users = User::factory()->count(3)->make();
-        //$response = $this->get('/');
-
-       // $response->assertStatus(200);
-        $this->assertGreaterThan(0,User::all()->count());        
-       // $this->assertDatabaseCount('users', 1);
-       $this->assertDatabaseHas('users', [
-        'mobile' =>  $mobile,
-        'email' => $email
-        ]);
-        $user = User::where('email', $email)->where('mobile', $mobile)->first();
-         $this->assertNotNull($user);
-        // $this->assertAuthenticatedAs($user);
+        return $user;
     }
 }

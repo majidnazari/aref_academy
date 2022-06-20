fba6c113ca86de9c40ebc653c15ccef77f78296a
majid nazari <majidnazarister@gmail.com>
2022-06-20 12:19:17 +0430

    add branch_id to academy

--- app/GraphQL/Queries/BranchClassRoom/GetBranchClassRooms.php
+++ app/GraphQL/Queries/BranchClassRoom/GetBranchClassRooms.php
@@ -20,7 +20,21 @@ final class GetBranchClassRooms
     }
     function resolveBranchClassRoomsAttribute($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) 
     {
-        $BranchClassRoom= BranchClassRoom::where('deleted_at', null);
+        //$BranchClassRoom= BranchClassRoom::where('deleted_at', null);
+        $BranchClassRoom=BranchClassRoom::where('deleted_at', null)
+                         ->whereHas('branch',function($query) use($args){
+                                    if(isset($args['branch_id'])){
+                                        $query->where('branches.id',$args['branch_id']);
+                                    }  
+                                    // if(isset($args['teacher_id'])){
+
+                                    //     $query->where('users1.id',$args['teacher_id']);
+                                        
+                                    // }
+                                    return true;  
+
+                          })
+                          ->with('branch');
         return $BranchClassRoom;
     }
 }

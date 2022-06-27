fb099a75905da7137f38ac9a37d435483a002c5d
majid nazari <majidnazarister@gmail.com>
2022-06-27 10:52:54 +0430

    edit crud for course student

--- app/GraphQL/Mutations/CourseStudent/CreateCourseStudent.php
+++ app/GraphQL/Mutations/CourseStudent/CreateCourseStudent.php
@@ -23,10 +23,23 @@ final class CreateCourseStudent
     public function resolver($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
     {
         $user_id = auth()->guard('api')->user()->id;
+        $course_exist= CourseStudent::where('course_id',$args['course_id'])
+        ->where('student_id',$args['student_id'])
+        ->first();
+        if($course_exist)
+        {
+            return $course_exist;
+        }
         $CourseStudente = [
             'course_id' => $args['course_id'],
             'student_id' => $args['student_id'],
-            'status' => $args['status'],
+            'manager_status' => isset($args['status']) ? $args['status'] : 'pending',
+            'financial_status' => isset($args['financial_status']) ? $args['financial_status'] : 'pending',
+            'student_status' => isset($args['student_status']) ? $args['student_status'] : 'ok',
+            'user_id_manager' => isset($args['user_id_manager']) ? $args['user_id_manager'] : 0,
+            'user_id_financial' => isset($args['user_id_financial']) ? $args['user_id_financial'] : 0,
+            'user_id_student_status' => isset($args['user_id_student_status']) ? $args['user_id_student_status'] : 0,
+            
             'user_id_creator' =>  $user_id,
             'user_id_approved' => 0            
         ];
--- app/GraphQL/Queries/CourseStudent/GetCourseStudents.php
+++ app/GraphQL/Queries/CourseStudent/GetCourseStudents.php
@@ -18,39 +18,38 @@ final class GetCourseStudents
     {
         // TODO implement the resolver
     }
-    function resolveCourseStudent($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) 
+    function resolveCourseStudent($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         //$CourseStudent= CourseStudent::where('deleted_at', null);//->orderBy('id','desc');
-        $CourseStudent=CourseStudent::where('deleted_at', null)
-                         ->whereHas('user_creator',function($query) use($args){
-                                    if(isset($args['user_id_creator'])){
-                                        $query->where('users.id',$args['user_id_creator']);
-                                    }                                     
-                                    return true;  
-
-                          })
-                          ->with('user_creator')
-                          ->whereHas('user_manager',function($query) use($args){
-                                    if(isset($args['user_id_manager']))
-                                        $query->where('users.id',$args['user_id_manager']);
-                                    else
-                                        return true;
-                          })
-                          ->with('user_manager')
-                          ->whereHas('user_financial',function($query) use($args){
-                            if(isset($args['user_id_financial']))
-                                $query->where('users.id',$args['user_id_financial']);
-                            else
-                                return true;
-                            })
-                            ->with('user_financial')
-                            ->whereHas('user_student_status',function($query) use($args){
-                                if(isset($args['user_id_student_status']))
-                                    $query->where('users.id',$args['user_id_student_status']);
-                                else
-                                    return true;
-                            })
-                            ->with('user_student_status');
+        $CourseStudent = CourseStudent::where('deleted_at', null)
+            ->where(function ($query) use ($args) {
+                if (isset($args['user_id_creator'])) {
+                    $query->where('users.id', $args['user_id_creator']);
+                }
+                return true;
+            })
+            ->with('user_creator')
+            ->where(function ($query) use ($args) {
+                if (isset($args['user_id_manager']))
+                    $query->where('users.id', $args['user_id_manager']);
+                else
+                    return true;
+            })
+            ->with('user_manager')
+            ->where(function ($query) use ($args) {
+                if (isset($args['user_id_financial']))
+                    $query->where('users.id', $args['user_id_financial']);
+                else
+                    return true;
+            })
+            ->with('user_financial')
+            ->where(function ($query) use ($args) {
+                if (isset($args['user_id_student_status']))
+                    $query->where('users.id', $args['user_id_student_status']);
+                else
+                    return true;
+            })
+            ->with('user_student_status');
         return $CourseStudent;
     }
 }

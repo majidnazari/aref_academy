21efb4c33a1532f0a4c52161bef6f077cda328e0
majid nazari <majidnazarister@gmail.com>
2022-07-09 16:44:23 +0430

    add year policy to academy

--- app/Policies/YearPolicy.php
+++ app/Policies/YearPolicy.php
@@ -3,9 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Year;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class UserPolicy
+class YearPolicy
 {
     use HandlesAuthorization;
     private $allowed_user=array("admin");
@@ -28,10 +29,10 @@ class UserPolicy
      * Determine whether the user can view the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param  \App\Models\Year  $year
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function view(User $user, User $model=null):bool
+    public function view(User $user, Year $year=null):bool
     {
         $user_role=auth()->guard('api')->user()->group->type;       
         if(in_array($user_role,$this->allowed_user))
@@ -45,7 +46,7 @@ class UserPolicy
      * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function create(User $user):bool
+    public function create(User $user)
     {
         $user_role=auth()->guard('api')->user()->group->type;       
         if(in_array($user_role,$this->allowed_user))
@@ -57,10 +58,10 @@ class UserPolicy
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param  \App\Models\Year  $year
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function update(User $user, User $model=null):bool
+    public function update(User $user, Year $year=null):bool
     {
         $user_role=auth()->guard('api')->user()->group->type;       
         if(in_array($user_role,$this->allowed_user))
@@ -72,10 +73,10 @@ class UserPolicy
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param  \App\Models\Year  $year
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function delete(User $user, User $model=null):bool
+    public function delete(User $user, Year $year=null):bool
     {
         $user_role=auth()->guard('api')->user()->group->type;       
         if(in_array($user_role,$this->allowed_user))
@@ -87,23 +88,23 @@ class UserPolicy
      * Determine whether the user can restore the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param  \App\Models\Year  $year
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function restore(User $user, User $model)
+    public function restore(User $user, Year $year)
     {
-        return false;
+        //
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param  \App\Models\Year  $year
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function forceDelete(User $user, User $model)
+    public function forceDelete(User $user, Year $year)
     {
-        return false;
+        //
     }
 }

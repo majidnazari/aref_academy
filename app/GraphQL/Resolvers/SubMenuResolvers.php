12e2b8b80c876a4a269b02170ace62b2190efaf9
majid nazari <majidnazarister@gmail.com>
2022-09-07 11:36:44 +0430

    remove test comment

--- app/GraphQL/Resolvers/SubMenuResolvers.php
+++ app/GraphQL/Resolvers/SubMenuResolvers.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\GraphQL\Resolvers;
 
 use App\Models\Group;
@@ -9,16 +10,17 @@ use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 use Log;
 
-class SubMenuResolvers {
+class SubMenuResolvers
+{
     /**
      * @param  \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder  $builder
      * @param  array<string, mixed>  $whereConditions
      */
     public function subMenuResolver($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        // TODO make calls to $builder depending on $whereConditions
-       $group_menue_ids= GroupMenu::where('group_id',$rootValue['pivot']['group_id'])->pluck("menu_id");
-       return Menu::where("parent_id",$rootValue['id'] )->whereIn('id',$group_menue_ids)->get();
+
+        $group_menue_ids = GroupMenu::where('group_id', $rootValue['pivot']['group_id'])->pluck("menu_id");
+
+        return Menu::where("parent_id", $rootValue['id'])->whereIn('id', $group_menue_ids)->get();
     }
 }
-

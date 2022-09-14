8337dd0e1919aa4b3eec19e1cd2f7fb87eac7c18
majid nazari <majidnazarister@gmail.com>
2022-09-14 14:38:52 +0430

    add crude test for student warning

---
+++ database/factories/StudentWarningFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use App\Models\StudentWarningHistory;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\StudentWarning>
+ */
+class StudentWarningFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $user=$this->faker->randomElement(User::pluck('id'));// User::factory()->create();
+        if($user===null)
+        {
+            $user= User::factory()->create();
+            $user=$user->id;
+        }
+        $course=$this->faker->randomElement(Course::pluck('id'));
+        if($course===null)
+        {
+            $course=Course::factory()->create();
+            $course=$course->id;
+        }
+        $student_warning_history=$this->faker->randomElement(StudentWarningHistory::pluck('id'));
+        if($student_warning_history===null){
+            $student_warning_history=StudentWarningHistory::factory()->create();
+            $student_warning_history=$student_warning_history->id;
+        }
+        
+        return [
+            "user_id_creator" =>$user,
+            "user_id_updator" => 0,
+            "student_id" => $this->faker->randomDigit,
+            "course_id" => $course,
+            "comment" => $this->faker->text(),
+            "student_warning_history_id" => $student_warning_history,
+
+        ];
+    }
+}
---
+++ tests/Feature/StudentWarningTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\StudentWarning;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StudentWarningTest extends TestCase
+{
+    use WithFaker;
+    //use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_getAllStudentWarning()
+    {
+        $count=rand(1,3);
+        $Fault_created = StudentWarning::factory($count)->create();
+      
+        $this->assertGreaterThanOrEqual($count,StudentWarning::all()->count());
+
+    }
+
+    public function test_createStudentWarning()
+    {
+        $student_warning_model=StudentWarning::factory()->make()->toArray();
+        StudentWarning::create($student_warning_model);
+        $this->assertDatabaseHas('student_warnings',$student_warning_model);
+    }
+    public function test_updateStudentWarning()
+    {
+        $student_warning_model=StudentWarning::factory()->make()->toArray();
+        StudentWarning::create($student_warning_model);
+        $student_warning_model_new=StudentWarning::factory()->make()->toArray();
+        $founded_student_warning_updated=StudentWarning::where($student_warning_model)->update($student_warning_model_new);
+
+        $this->assertDatabaseHas('student_warnings',$student_warning_model_new);
+    }
+    public function test_deleteStudentWarning()
+    {
+        $student_warning_model=StudentWarning::factory()->make()->toArray();
+        $student_warning_created=StudentWarning::create($student_warning_model);
+        $student_warning_created->delete();
+        //$founded_student_warning_updated=StudentWarning::where($student_warning_model)->delete();
+        //$this->assertSoftDeleted($student_warning_created);
+        //$this->assertTrue(StudentWarning::where($student_warning_created)->exists());
+        $this->assertDatabaseMissing('student_warnings', $student_warning_model);
+       //if
+        // if(!StudentWarning::where($student_warning_created)->first())
+        // {
+        //     $this->assertTrue(true);
+        // }
+
+    }
+}

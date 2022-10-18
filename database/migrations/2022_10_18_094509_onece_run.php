76e07f9fd91a8fd60a07466d23703c2a145dc448
majid nazari <majidnazarister@gmail.com>
2022-10-18 10:10:25 +0330

    add not registered migration

---
+++ database/migrations/2022_10_18_094509_onece_run.php
@@ -0,0 +1,69 @@
+<?php
+
+use App\Models\AbsencePresence;
+use App\Models\Course;
+use App\Models\CourseSession;
+use App\Models\CourseStudent;
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // $course_session_date=strtotime($get_course_session->start_date .' '. $get_course_session->end_time);
+        // $now=strtotime(date("Y-m-d H:i:s"));
+        $current_date=Carbon::now()->format('Y-m-d');
+        $current_time=Carbon::now()->format('H:i:s');
+        //Log::info("current date is: " . $current_date . " current time is" . $current_time);
+        $all_course=Course::pluck('id');
+
+        $all_course_session_ids_of_this_course = CourseSession::whereIn('course_id', $all_course)
+        ->where('start_date','<',$current_date )
+        ->orWhere(function($query) use($current_date,$current_time){
+            $query->where('start_date','=',$current_date)
+            ->where('end_time','<',$current_time);
+        })
+       
+        ->pluck('id');
+        $cout_session=0;
+        foreach ($all_course_session_ids_of_this_course as $course_session_id) {
+            $cout_session++;
+            $students = AbsencePresence::where('course_session_id', $course_session_id)
+                ->pluck('student_id');
+            $get_all_new_course_student = CourseStudent:://where('course_id', $course_id)->
+                whereNotIn('student_id', $students)
+                ->get();
+                foreach ($get_all_new_course_student as $student) {
+                    $s_id = $student->student_id;
+                    $AbsencePresence = [
+                        'user_id_creator' => 1,
+                        "course_session_id" => $course_session_id,
+                        "teacher_id" => 0,
+                        "student_id" => $student->student_id,
+                        'status' => "not_registered"
+        
+                    ];
+                    $AbsencePresence = AbsencePresence::create($AbsencePresence);
+                }
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};

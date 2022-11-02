71a2e04eb8de6b9757d31d26b49258b41416e66e
majid nazari <majidnazarister@gmail.com>
2022-11-02 12:35:04 +0330

    delete all of not registered  and no action sessions of course session

--- app/GraphQL/Mutations/CourseSession/DeleteCourseSession.php
+++ app/GraphQL/Mutations/CourseSession/DeleteCourseSession.php
@@ -2,7 +2,9 @@
 
 namespace App\GraphQL\Mutations\CourseSession;
 
+use App\Models\AbsencePresence;
 use App\Models\CourseSession;
+use Carbon\Carbon;
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
@@ -10,6 +12,7 @@ use Joselfonseca\LighthouseGraphQLPassport\Events\PasswordUpdated;
 use Joselfonseca\LighthouseGraphQLPassport\Exceptions\ValidationException;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use GraphQL\Error\Error;
+use Log;
 
 final class DeleteCourseSession
 {
@@ -23,15 +26,67 @@ final class DeleteCourseSession
     }
     public function resolver($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
     {
-        $user_id=auth()->guard('api')->user()->id;
-        $args["user_id_creator"]=$user_id;
-        $CourseSession=CourseSession::find($args['id']);
-
-        if(!$CourseSession)
-        {
-                return Error::createLocatedError("COURSESESSION-DELETE-RECORD_NOT_FOUND");
-        }
+        $current_date=Carbon::now()->format('Y-m-d');
+        $current_time=Carbon::now()->format('H:i:s');
+        $error_msg="";
         
+        $user_id = auth()->guard('api')->user()->id;
+        $args["user_id_creator"] = $user_id;
+        $CourseSession = CourseSession::find($args['id']);
+      
+        //Log::info("the find is:" . ($CourseSession));
+
+        if (!$CourseSession) {
+            return Error::createLocatedError("COURSESESSION-DELETE-RECORD_NOT_FOUND");
+        }
+        // $empty_absence_presence = AbsencePresence::where('course_session_id', $CourseSession->id)
+        // ->where(function($query)  {
+        //     $query->whereNotIn('status',['not_registered', 'noAction']);
+            
+        //     //->orWhere('status', '!=', 'noAction');
+        // })->count();
+
+        $empty_absence_presence = AbsencePresence::where('course_session_id', $CourseSession->id)
+        ->where(function($query)  {
+            $query->whereNotIn('status',['not_registered', 'noAction']);
+            
+            //->orWhere('status', '!=', 'noAction');
+        })
+        ->WhereHas('courseSession',function($q) use ($current_date, $current_time) {
+            $q->where('start_date','<',$current_date )
+            ->orWhere(function($query) use($current_date,$current_time){
+                $query->where('start_date','=',$current_date)
+                ->where('end_time','<',$current_time);
+            }) ;
+        })
+        ->with('courseSession')
+        ->count();
+
+        // ->where(function ($q) use ($current_date, $current_time) {
+        //     $q->where('start_date','<',$current_date )
+        //     ->orWhere(function($query) use($current_date,$current_time){
+        //         $query->where('start_date','=',$current_date)
+        //         ->where('end_time','<',$current_time);
+        //     }) ;
+        // });      
+
+        // $all_course_session_ids_of_this_course = CourseSession::
+        // where('course_id', $course_id)
+        // ->where(function ($q) use ($current_date, $current_time) {
+        //     $q->where('start_date','<',$current_date )
+        //     ->orWhere(function($query) use($current_date,$current_time){
+        //         $query->where('start_date','=',$current_date)
+        //         ->where('end_time','<',$current_time);
+        //     }) ;
+        // })      
+        // ->pluck('id');
+           
+           
+        //Log::info("the count of all except  not_R and noA is:" . $empty_absence_presence);
+        if ($empty_absence_presence > 0) {
+
+            return Error::createLocatedError("COURSESESSION-DELETE-IT_IS_USED_BEFORE");
+        }
         // if(!$CourseSession)
         // {
         //     return [
@@ -40,9 +95,7 @@ final class DeleteCourseSession
         //     ];
         // }
         $CourseSession_result= $CourseSession->delete();        
-       
+
         return $CourseSession;
-        
     }
-    
 }

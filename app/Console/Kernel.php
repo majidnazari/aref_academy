a31e22bbd39a6da5f97380260c4b44c5df3ba76b
majid nazari <majidnazarister@gmail.com>
2022-12-05 12:40:38 +0330

    remove  telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('telescope:prune')->daily();
+        //$schedule->command('telescope:prune')->daily();
     }
 
     /**

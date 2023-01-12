36add0c88ec922af4ac7982d4ad856abeae4a4be
majid nazari <majidnazarister@gmail.com>
2023-01-12 14:57:41 +0330

    limit auth token time

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -34,7 +34,9 @@ class AuthServiceProvider extends ServiceProvider
         
         Passport::routes();
 
-        Passport::tokensExpireIn(now()->addDays(15));
+        //Passport::tokensExpireIn(now()->addDays(15)); 
+        Passport::tokensExpireIn(now()->addMinutes(20)); 
+        Passport::refreshTokensExpireIn(now()->addHours(1));
         // Passport::refreshTokensExpireIn(now()->addDays(30));
         //Passport::personalAccessTokensExpireIn(now()->addMonths(6));
 

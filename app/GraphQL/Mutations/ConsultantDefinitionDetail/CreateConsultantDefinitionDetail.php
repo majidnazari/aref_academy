07b482787f4651d2f378f833197f3cdd2f6e9684
majid nazari <majidnazarister@gmail.com>
2023-10-28 15:01:05 +0330

    add copy consultant timesheet weekly from this week to next week

--- app/GraphQL/Mutations/ConsultantDefinitionDetail/CreateConsultantDefinitionDetail.php
+++ app/GraphQL/Mutations/ConsultantDefinitionDetail/CreateConsultantDefinitionDetail.php
@@ -11,6 +11,7 @@ use Exception;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use GraphQL\Error\Error;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Log;
 
 final class CreateConsultantDefinitionDetail
@@ -29,8 +30,8 @@ final class CreateConsultantDefinitionDetail
         $now = Carbon::now();
         $user_id = auth()->guard('api')->user()->id;
         $branch_id = auth()->guard('api')->user()->branch_id;
-        $branch_class_room_ids= BranchClassRoom::where('branch_id',$branch_id)->pluck('id');
-       // Log::info($branch_class_room_ids);
+        $branch_class_room_ids = BranchClassRoom::where('branch_id', $branch_id)->pluck('id');
+        // Log::info($branch_class_room_ids);
 
         $start_hour = $args['start_hour'];
         $end_hour = $args['end_hour'];
@@ -38,7 +39,7 @@ final class CreateConsultantDefinitionDetail
         $error = [];
 
         $all_of_consultant_data_collection = ConsultantDefinitionDetail::where('consultant_id', $args['consultant_id'])
-        ->whereIn('branch_class_room_id', $branch_class_room_ids);
+            ->whereIn('branch_class_room_id', $branch_class_room_ids);
         $all_of_consultant_data = $all_of_consultant_data_collection->get();
         // ->where('user_id', $user_id)
         // ->with('branchClassRoom')
@@ -178,36 +179,144 @@ final class CreateConsultantDefinitionDetail
         }
     }
 
-    private function validateSession($all_of_consultant_data_collection,  $consultant_definition_detail_date)
+    public function validateSession($all_times_of_next_week_collection,  $target_session_time_want_to_check_next_week)
     {
-        $is_exist="";
-        $is_exist =  $all_of_consultant_data_collection  
+
+        $all_times_of_next_week_collection_copy=clone $all_times_of_next_week_collection;
+        //Log::info("inner all are:" . json_encode($all_times_of_next_week_collection));
+        // Log::info("inner target is:" . json_encode($target_session_time_want_to_check_next_week));
+        $target_session_date = $target_session_time_want_to_check_next_week;
+        $is_exist = "";
+        $is_exist =  $all_times_of_next_week_collection_copy
             //->where('branch_class_room_id', $consultant_definition_detail_date['branch_class_room_id'])         
-            ->where('consultant_id', $consultant_definition_detail_date['consultant_id'])
-            ->where('session_date', $consultant_definition_detail_date['session_date'])
-            ->where('start_hour', $consultant_definition_detail_date['start_hour'])
-            ->where('end_hour', $consultant_definition_detail_date['end_hour'])
-            ->orWhere(function ($query) use ($consultant_definition_detail_date) {
+            ->where('consultant_id', $target_session_date['consultant_id'])
+            ->where('session_date', $target_session_date['session_date'])
+            ->where('start_hour', $target_session_date['start_hour'])
+            ->where('end_hour', $target_session_date['end_hour'])
+            ->orWhere(function ($query) use ($target_session_date) {
 
-                $query->where('start_hour', '<=', $consultant_definition_detail_date['start_hour'])
-                    ->where('end_hour', '>=', $consultant_definition_detail_date['end_hour'])
-                    ->where('consultant_id', $consultant_definition_detail_date['consultant_id']);
+                $query->where('start_hour', '<=', $target_session_date['start_hour'])
+                    ->where('end_hour', '>=', $target_session_date['end_hour'])
+                    ->where('consultant_id', $target_session_date['consultant_id'])
+                    ->where('session_date', $target_session_date['session_date']);
             })
-            ->orWhere(function ($query) use ($consultant_definition_detail_date) {
+            ->orWhere(function ($query) use ($target_session_date) {
 
-                $query->where('start_hour', '>=', $consultant_definition_detail_date['start_hour'])
-                    ->where('end_hour', '<=', $consultant_definition_detail_date['end_hour'])
-                    ->where('consultant_id', $consultant_definition_detail_date['consultant_id']);
+                $query->where('start_hour', '>=', $target_session_date['start_hour'])
+                    ->where('end_hour', '<=', $target_session_date['end_hour'])
+                    ->where('consultant_id', $target_session_date['consultant_id'])
+                    ->where('session_date', $target_session_date['session_date']);
             })
-            ->orWhere(function ($query) use ($consultant_definition_detail_date) {
+            ->orWhere(function ($query) use ($target_session_date) {
 
-                $query->where('end_hour', '>', $consultant_definition_detail_date['start_hour'])
-                    ->where('start_hour', '<', $consultant_definition_detail_date['end_hour'])
-                    ->where('consultant_id', $consultant_definition_detail_date['consultant_id']);
+                $query->where('end_hour', '>', $target_session_date['start_hour'])
+                    ->where('start_hour', '<', $target_session_date['end_hour'])
+                    ->where('consultant_id', $target_session_date['consultant_id'])
+                    ->where('session_date', $target_session_date['session_date']);
             })
             ->first();
-        
-        //Log::info($is_exist);
+
+         //Log::info("validateSession is:" . json_encode($is_exist));
         return  $is_exist;
+    }
+
+    public function copyWeekTimeTable($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $user_id = auth()->guard('api')->user()->id;
+        $consultant_id = $args['consultant_id'];
+        $error = [];
+        $start_hours = [];
+        $end_hours = [];
+        $session_date = [];
+        $data = [];
+
+        $startOfWeek =  Carbon::now()->startOfWeek(Carbon::SATURDAY)->format("Y-m-d");
+        $endOfWeek =  Carbon::now()->startOfWeek(Carbon::SATURDAY)->addDays(6)->format("Y-m-d");
+
+        $getAllOfConsultantTimeTableCurrentWeeks = ConsultantDefinitionDetail::where('consultant_id', $consultant_id)
+            ->where('session_date', '>=',  $startOfWeek)
+            ->where('session_date', '<=', $endOfWeek)
+            ->orderBy('session_date', 'desc')
+            ->get();
+
+        $getAllDaysOfCurrentWeekHasPlaned = ConsultantDefinitionDetail::where('consultant_id', $consultant_id)
+            ->where('session_date', '>=',  $startOfWeek)
+            ->where('session_date', '<=', $endOfWeek)
+            ->orderBy('session_date', 'asc')
+            ->distinct()
+            ->pluck('session_date');
+        // ->get();
+
+        //Log::info("getAllDaysOfCurrentWeekHasPlaned is:" .  $getAllDaysOfCurrentWeekHasPlaned);
+
+        $getAllOfConsultantTimeTableNextWeeksQueryBuilder = ConsultantDefinitionDetail::where('consultant_id', $consultant_id)
+            ->where('session_date', '>=', Carbon::parse($startOfWeek)->addDays(7)->format("Y-m-d"))
+            ->where('session_date', '<=', Carbon::parse($endOfWeek)->addDays(7)->format("Y-m-d"));
+        // ->get();
+
+       // Log::info("getAllOfConsultantTimeTableNextWeeksQueryBuilder is:" .  json_encode($getAllOfConsultantTimeTableNextWeeksQueryBuilder->get()));
+
+        //$getAllOfConsultantTimeTableNextWeeks = $getAllOfConsultantTimeTableNextWeeksQueryBuilder->get();
+
+        foreach ($getAllDaysOfCurrentWeekHasPlaned as $one_day) {
+
+            $next_week_of_this_current_day = Carbon::parse($one_day)->addDays(7)->format("Y-m-d");
+
+            $current_day_start_times = $getAllOfConsultantTimeTableCurrentWeeks->where('session_date', $one_day)
+            ->pluck('start_hour')            
+            ->toArray();
+            
+            //Log::info("current times are:" . json_encode($current_day_start_times));
+
+            $current_day_end_times = $getAllOfConsultantTimeTableCurrentWeeks->where('session_date', $one_day)
+            ->pluck('end_hour')
+            ->toArray();
+
+
+            $current_day_class_id = $getAllOfConsultantTimeTableCurrentWeeks->where('session_date', $one_day)
+            ->pluck('branch_class_room_id')
+            ->toArray();
+            $steps = $getAllOfConsultantTimeTableCurrentWeeks->where('session_date', $one_day)
+            ->pluck('step')
+            ->toArray();
+
+            $combined_start_end_times = array_map(null,$current_day_start_times,  $current_day_end_times,$current_day_class_id, $steps);
+
+
+            //$combined_start_end_times = array_combine($current_day_start_times,  $current_day_end_times);
+
+            foreach ($combined_start_end_times as [$start , $end,$class_id, $steps]) {
+
+                $session_to_check_in_next_week = [
+                    "consultant_id" => $consultant_id,
+                    "start_hour" => $start,
+                    "end_hour" => $end,
+                    "session_date" => $next_week_of_this_current_day,
+                    "branch_class_room_id" => $class_id,
+                    "user_id" => $user_id,
+                    "step" =>  $steps
+
+                ];
+
+                //Log::info("target  is:" . json_encode($session_to_check_in_next_week));
+
+                //Log::info("all  are :" . json_encode($getAllOfConsultantTimeTableNextWeeksQueryBuilder->get()));
+
+                $is_exist = $this->validateSession($getAllOfConsultantTimeTableNextWeeksQueryBuilder,  $session_to_check_in_next_week);
+
+                 //Log::info("is_exist  is:" . json_encode($is_exist));
+
+                if ($is_exist) {
+                    $error[] = $session_to_check_in_next_week;
+                }
+
+                $data[] = $session_to_check_in_next_week;
+            }
+        }
+        if ($error) {           
+            return $error;
+        }        
+        $copy_definition_details_time_tables=ConsultantDefinitionDetail::insert($data);
+        return null;
     }
 }
